049345d15661ccbc1d22a787f49d6caa614d6ff8
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2024-04-08 14:06:23 +0900

    update for deployment

--- greetings.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("vent-bot")
-st.write("Vent anything here")
-name = st.text_input(label="name input",placeholder="Enter your name", max_chars=50)
-if st.button('Submit'):
-    st.write('Hi ' + name + ". What do you want to talk about today?")
